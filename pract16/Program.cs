5d43a596d92b02deb6dc17e700b58020a1f945b0
Abai <abai2010@bk.ru>
2017-12-24 05:33:22 +0600

    finished

---
+++ pract16/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace pract16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XmlDocument xDoc = new XmlDocument();
+            xDoc.Load("../../Users.xml");
+
+            // получим корневой элемент
+            XmlElement xRoot = xDoc.DocumentElement;
+            Console.WriteLine(xRoot.Name);
+
+            foreach (XmlNode xNode in xRoot)
+            {
+                Console.WriteLine("  {0}", xNode.Name);
+                foreach (XmlNode childNode in xNode.ChildNodes)
+                {
+                    Console.WriteLine("    {0}", childNode.Name);
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
